733c3964b1de756a3622aa6d020392bc14c1c25a
Leonardo <leo.del.lama@gmail.com>
2019-04-29 01:22:05 -0300

    Sprites do Player + atirando de maneira bizarra (mas pelo menos ta atirando)

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -12,35 +12,40 @@ public class WeaponController : MonoBehaviour {
     
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         //if (!playerLife.GetDead()) {
-            // Calculates direction from object to cursor
-            Vector3 d = cam.ScreenToWorldPoint(Input.mousePosition) - weapon.position;
-            d.Normalize();
+        // Calculates direction from object to cursor
+        Vector3 theScale = weapon.localScale;
+        theScale.x = transform.localScale.x;
+        weapon.localScale = theScale;
 
-            Vector2 direction = new Vector2(d.x, d.y);
+        Vector3 d = cam.ScreenToWorldPoint(Input.mousePosition) - weapon.position;
+        d.Normalize();
+
+        Vector2 direction = new Vector2(d.x, d.y) * transform.localScale.x;
         direction.Normalize();
 
-            // Verify player orientation
+        // Verify player orientation
         float angle = (player.transform.localScale.x <= -1) ?
-                Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg
-                : Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg
+            : Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            // Calculates new rotation
-            Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Calculates new rotation
+        Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-            // Spherically sets rotation
-            weapon.rotation = Quaternion.Slerp(weapon.rotation, newRotation, rotationSpeed * Time.deltaTime);
-            //Debug.Log(weapon.rotation.eulerAngles);
+        // Spherically sets rotation
+        weapon.rotation = Quaternion.Slerp(weapon.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        //Debug.Log(weapon.rotation.eulerAngles);
 
-            // Attacks on mouse click
-            if (Input.GetMouseButton(0))
-            {
-            //animator.ResetTrigger("Attacking");
-            //animator.SetTrigger("Attacking");
-            Quaternion bRotation = weapon.rotation;
-            gameObject.GetComponent<Shoot>().SpawnBullet(firePoint.position, bRotation);
-            }
+        // Attacks on mouse click
+        if (Input.GetMouseButton(0))
+        {
+        //animator.ResetTrigger("Attacking");
+        //animator.SetTrigger("Attacking");
+        Quaternion bRotation = weapon.rotation;
+        gameObject.GetComponent<Shoot>().SpawnBullet(firePoint.position, bRotation);
+        }
         //}
     }
 }
