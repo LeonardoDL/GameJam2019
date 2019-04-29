7a38c1c48d1dce7f6d61659f15a68c82f17ce3b0
Leonardo <leo.del.lama@gmail.com>
2019-04-29 20:36:51 -0300

    Random Sentence

---
+++ Assets/Scripts/RandomSentence.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSentence : MonoBehaviour
+{
+    public Text t;
+    public string[] txts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t.text = txts[Random.Range(0, txts.Length)];
+    }
+}
